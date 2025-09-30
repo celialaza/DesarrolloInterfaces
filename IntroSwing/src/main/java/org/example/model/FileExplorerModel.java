a1794f217fac164fd809955a6df78f62daf02be8
celialaza <celia.s234913@cesurformacion.com>
2025-09-30 18:59:09 +0200

    Update FileExplorerModel.java

--- IntroSwing/src/main/java/org/example/model/FileExplorerModel.java
+++ IntroSwing/src/main/java/org/example/model/FileExplorerModel.java
@@ -6,16 +6,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
+//En el modelo está la inteligencia del programa. ->Acceder al sistema de archivos.
+
 public class FileExplorerModel {
 
     public ArrayList<String> listarArchivos(String ruta) {
         var salida = new ArrayList<String>();
-        Path path = Path.of(ruta);
+        Path path = Path.of(ruta); //Convierte el texto a ruta
 
         if (Files.isDirectory(path)) {
             try {
-                for (Path p : Files.list(path).toList()) {
-                    salida.add(p.getFileName().toString());
+                for (Path p : Files.list(path).toList()) { //Files.list(path) Lista archivos en esa ruta                    salida.add(p.getFileName().toString());
                 }
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
